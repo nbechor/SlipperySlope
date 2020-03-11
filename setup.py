6df538dabecaabdca952b5d0c86a64d920222aa1
Noa Bechor <noa.bechor@gmail.com>
2020-03-11 15:42:16 -0400

    Generated initial project from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict icy sidewalks',
+    author='Noa Bechor',
+    license='MIT',
+)
